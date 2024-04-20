fe4a5385eef24054e31fe778c81f126fcfa576ab
JongCheol Moon <musikking1@gmail.com>
2024-04-20 14:27:32 +0900

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "varam_Rac"
+include(":app")
+ 
